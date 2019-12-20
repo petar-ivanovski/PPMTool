e668fb1d24c06df1034387578e498e782144611b
petar <petar.ivanovski@asseco-see.mk>
2019-12-20 10:55:04 +0100

    blabla

---
+++ ppmtool/src/main/java/com/course/ppmtool/repositories/BacklogRepository.java
@@ -0,0 +1,12 @@
+package com.course.ppmtool.repositories;
+
+import com.course.ppmtool.domain.Backlog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BacklogRepository extends CrudRepository<Backlog, Long> {
+
+    Backlog findByProjectIdentifier(String Identifier);
+
+}
